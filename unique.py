c7ff1e49e8f780fc786804bf1ec0c30038ee4f14
Coralsnake2404 <159200012+Coralsnake2404@users.noreply.github.com>
2024-02-20 17:57:54 -0500

    Add files via upload

---
+++ unique.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+     doors = []
+     for i in range(1,50):
+         randdoor = random.randrange(1,50)
+         while randdoor in doors:
+                 randdoor = random.randrange(1,50)
+         doors.append(randdoor)
+     print(doors)
+
+main()
