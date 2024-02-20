b25901e7040292b781d321e48163c93a57fe653a
Camilavgiraldo <54727592+Camilavgiraldo@users.noreply.github.com>
2024-02-20 18:26:49 -0500

    Add files via upload

--- unique.py
+++ unique.py
@@ -1,10 +1,20 @@
-import random
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 18:07:43 2024
 
+@author: lab40-204-4
+"""
+
+import random as camy
+
+random_nums = []
 def run():
-     doors = []
-     for i in range(1,50):
-         randdoor = random.randrange(1,50)
-         while randdoor in doors:
-                 randdoor = random.randrange(1,50)
-         doors.append(randdoor)
-     print(doors)
+    for i in range(50):
+        x = camy.randint(1, 1000)
+        while x in random_nums:
+            x =  camy.randint(1, 1000)
+        random_nums.append(x)
+    return random_nums
+    
+print(run())
+    
