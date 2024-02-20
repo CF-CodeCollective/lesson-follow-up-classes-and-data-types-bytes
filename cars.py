2b5a4ee1da937a60d1ef66263f836b16614bff17
Camilavgiraldo <54727592+Camilavgiraldo@users.noreply.github.com>
2024-02-20 17:44:34 -0500

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,13 +1,19 @@
-class car:
-    def __init__(self, make, model, year):
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class car():
+    def __init__(self, year, make, model):
+        self.year = year
         self.make = make
         self.model = model
-        self.year = year
-
+        
     def info(self):
         return self.year, self.make, self.model
-
-if __name__ == "__main__":
-    vehicle = car("Kia", "Sportage", "2014")
-    x, y, z = vehicle.info()
-    print(f"This car is a {x} {y} {z}.")
+    
+kia = car(2014, "Kia", "Sportage")
+year, make, model = kia.info()
+print(f"This car is a {year} {make} {model}")
