3b9a8fbb889d0841d56088058a171e15b9827162
Camilavgiraldo <54727592+Camilavgiraldo@users.noreply.github.com>
2024-02-20 18:02:09 -0500

    Add files via upload

---
+++ registry.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 17:45:42 2024
+
+@author: lab40-204-4
+"""
+
+
+names = {
+    "Emanuel" : 123,
+    "Colt": 526,
+    "Gavin": 2784,
+    "Layla": 235,
+    "Kianna": 98765,
+    "Jaydan": 236,
+    "Shaniya": 7934,
+    "Amanda": 427,
+    "Joseph": 247,
+    "Jazlyn": 2945,
+    "Luke": 23880,
+    "Blaine": 5
+    }
+
+def run(name):
+    return names[name]
