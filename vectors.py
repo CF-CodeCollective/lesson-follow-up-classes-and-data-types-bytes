b6405f1266b0f81fd8f6285120eadb88ccb8761d
Barney Tanczos <116983214+T-Barney@users.noreply.github.com>
2024-02-20 23:04:02 -0500

    Add files via upload

---
+++ vectors.py
@@ -0,0 +1,20 @@
+class vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def add(self, other):
+        return vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def subtract(self, other):
+        return vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return vector3(self.y * other.z - self.z * other.y, self.z * other.x - self.x * other.z, self.x * other.y - self.y * other.x)
+
+    def run(self):
+        return f"({self.x}, {self.y}, {self.z})"
