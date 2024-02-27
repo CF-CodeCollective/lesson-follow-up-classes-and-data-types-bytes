90c8299c0353a25f635407bf155183414b8e6bb3
Barney Tanczos <116983214+T-Barney@users.noreply.github.com>
2024-02-27 17:50:08 -0500

    Add files via upload

--- vectors.py
+++ vectors.py
@@ -14,7 +14,7 @@ class Vector3:
         return a.x * b.x + a.y * b.y + a.z * b.z
 
     def cross(a, b):
-        return Vector3(a.y * b.z - a.z * b.y, -(a.z * b.x - a.x * b.z), a.x * b.y - a.y * b.x)
+        return Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
     
     def __str__(self):
         return f"<{self.x}, {self.y}, {self.z}>"
