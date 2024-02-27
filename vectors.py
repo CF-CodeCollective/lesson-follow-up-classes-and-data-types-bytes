e47fa2bade60fd75284eaeab844f5b97cd9763f3
Barney Tanczos <116983214+T-Barney@users.noreply.github.com>
2024-02-27 17:45:15 -0500

    Add files via upload

--- vectors.py
+++ vectors.py
@@ -17,4 +17,4 @@ class Vector3:
         return Vector3(a.y * b.z - a.z * b.y, -(a.z * b.x - a.x * b.z), a.x * b.y - a.y * b.x)
     
     def __str__(self):
-        return "<{self.x}, {self.y}, {self.z}>"
+        return f"<{self.x}, {self.y}, {self.z}>"
