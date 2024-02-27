bd59cd5b63e1aa610b8a58a8fbc299ed5ce04a9a
Barney Tanczos <116983214+T-Barney@users.noreply.github.com>
2024-02-27 17:54:42 -0500

    Add files via upload

--- unique.py
+++ unique.py
@@ -1,20 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 20 18:07:43 2024
+import random
 
-@author: lab40-204-4
-"""
-
-import random as camy
-
-random_nums = []
 def run():
-    for i in range(50):
-        x = camy.randint(1, 1000)
-        while x in random_nums:
-            x =  camy.randint(1, 1000)
-        random_nums.append(x)
-    return random_nums
-    
-print(run())
-    
+    num = set()
+    while len(num) < 50:
+        num.add(random.randint(1, 1000))
+
+    return num
+
+if __name__ == "__main__":
+    print(run())
