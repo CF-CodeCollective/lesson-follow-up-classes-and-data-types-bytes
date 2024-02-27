b0324e32268d396bd462f5cb7e3df21f473be417
BadAtBest <126367475+BadAtBest@users.noreply.github.com>
2024-02-27 17:55:45 -0500

    Add files via upload

--- vectors.py
+++ vectors.py
@@ -1,20 +1,20 @@
-class Vector3:
+class Vector3():
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-
+    
     def add(a, b):
         return Vector3(a.x + b.x, a.y + b.y, a.z + b.z)
-
+        
     def subtract(a, b):
         return Vector3(a.x - b.x, a.y - b.y, a.z - b.z)
-
+        
     def dot(a, b):
-        return a.x * b.x + a.y * b.y + a.z * b.z
-
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
+                       
     def cross(a, b):
-        return Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+        return Vector3((a.y * b.z) - (b.y * a.z), -((a.x * b.z) - (b.x * a.z)), (a.x * b.y) - (b.x * a.y))
     
     def __str__(self):
-        return f"<{self.x}, {self.y}, {self.z}>"
+        return f"<{self.x},{self.y},{self.z}>"
